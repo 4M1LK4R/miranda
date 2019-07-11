be34d3111588a76a65975c94b90f3b7baf2f6773
4M1LK4R <amilkar.dominguez@gmail.com>
2019-07-11 16:07:57 -0400

    TypesCaloguesTableSeeder

---
+++ database/seeds/TypesCatalogueTableSeeder.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TypesCatalogueTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+
+        App\TypeCatalog::create([
+            'name' => 'TIPO',
+            'description' => 'Tipo de Productos.',
+            'state' => 'ACTIVO'
+        ]);
+        
+        App\TypeCatalog::create([
+            'name' => 'ALMACEN',
+            'description' => 'Almacen de Productos.',
+            'state' => 'ACTIVO'
+        ]);
+
+        App\TypeCatalog::create([
+            'name' => 'DEPARTAMENTO',
+            'description' => 'Departamentos de Bolivia.',
+            'state' => 'ACTIVO'
+        ]);
+
+        App\TypeCatalog::create([
+            'name' => 'LINEA',
+            'description' => 'linea de Productos.',
+            'state' => 'ACTIVO'
+        ]);
+
+        App\TypeCatalog::create([
+            'name' => 'TIPO CLIENTE',
+            'description' => 'Tipos de clientes.',
+            'state' => 'ACTIVO'
+        ]);
+        
+        App\TypeCatalog::create([
+            'name' => 'INDUSTRIA',
+            'description' => 'Industria de Productos.',
+            'state' => 'ACTIVO'
+        ]);
+        ///// duda son necesarios ???
+        App\TypeCatalog::create([
+            'name' => 'ESTADO PAGO',
+            'description' => 'Estados de pagos.',
+            'state' => 'ACTIVO'
+        ]);
+        App\TypeCatalog::create([
+            'name' => 'TIPO PAGO',
+            'description' => 'Tipo de pago que se utiliza.',
+            'state' => 'ACTIVO'
+        ]);
+    }
+}
