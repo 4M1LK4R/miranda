0b5f9925cf4300ef82d7c36383977dd9bc6301d6
leoOSX <agusosx@gmail.com>
2019-08-12 12:32:58 -0400

    *

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
          $this->call(UsersTableSeeder::class);
          $this->call(CatalogTypesTableSeeder::class);
          $this->call(CataloguesTableSeeder::class);
+         $this->call(ProductsTableSeeder::class);
          $this->call(TestTableSeeder::class);
+         
     }
 }
